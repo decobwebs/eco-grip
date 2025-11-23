bc15bc6c741bef79989cf5fdd3ec2a995f58b8e6
decobwebs <cobwebb784@gmail.com>
2025-11-24 00:41:00 +0100

    first commit

---
+++ staff-data.js
@@ -0,0 +1,14 @@
+const staffDatabase = {
+  "0012": {
+    name: "Emmanuel Ekokobe",
+    role: "MD/ CEO",
+    status: "Active",
+    image: "images/0012.png",
+  },
+  "0013": {
+    name: "Favour Boi Oluchi",
+    role: "Team Lead, Admin Services",
+    status: "Active",
+    image: "images/0013.png",
+  },
+};
